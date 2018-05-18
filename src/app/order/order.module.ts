643240c2555bb30be35c0e76f42ae74fdb8dc234
Rafael Matos <skyrafael@hotmail.com>
2018-05-18 07:48:03 -0300

    inicial commit

---
+++ src/app/order/order.module.ts
@@ -0,0 +1,18 @@
+
+import {NgModule} from '@angular/core';
+import {OrderComponent} from './order.component';
+import {OrderItemsComponent} from './order-items/order-items.component';
+import {SharedModule} from '../shared/shared.module';
+import {RouterModule, Routes} from '@angular/router';
+import {DeliveryCostsComponent} from './delivery-costs/delivery-costs.component';
+
+const ROUTES: Routes = [
+  {path: '', component: OrderComponent}
+]
+
+@NgModule({
+  declarations: [OrderComponent, OrderItemsComponent, DeliveryCostsComponent],
+  imports: [SharedModule, RouterModule.forChild(ROUTES)]
+})
+
+export class OrderModule {}
